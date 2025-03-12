028604be8635ffd1cb39706184d96b47839727b7
vonzimr <vonzimr@gmail.com>
2025-03-11 21:56:52 -0700

    add site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
+  site: "https://rvonzimr.github.io/furwood_campout",
   vite: {
     plugins: [tailwindcss()],
   },
